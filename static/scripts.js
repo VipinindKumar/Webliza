3c89d9ba9e6c5bd3f6ea81befebf4ce344ea4387
vipin kumar <vipin3432@gmail.com>
2017-01-04 17:58:48 +0000

    Add comments and more style for improved readability

--- static/scripts.js
+++ static/scripts.js
@@ -1,20 +1,28 @@
 
 $(document).ready(function(){
     $('#form-box').submit(function(event){
+        
+        //make the typed message show in the box
         $('#box').append('<div class="user"><p>' + $('#message-box').val() + '</p></div><p class="space"></p>');
         
         var parameters = {
             message: $('#message-box').val()
         };
         
+        //ajax is used to get the reply via post, after the completion of the ajax request reset function is called
         $.when( $.post('/message', parameters, function(data) {
+            
+            //show the reply in the box
             $('#box').append('<div class="eliza"><p>' + data + '</p></div><p></p>');
         }, 'text')).done(function reset() {
+                
+                // scroll to bottom using animation
                 $("#box").animate({scrollTop:$("#box")[0].scrollHeight}, 1000);
+                
+                // empty the input box
                 $('#message-box').val('').focus();
             });
-        
-        
+            
         return false;
     });
 });
